f6b2693eb1190ade46e29d4a811766648c2c690a
Ray Anthony <62559681+DollieSon@users.noreply.github.com>
2024-04-29 00:14:46 +0800

    Added Specific Day Counter on Specific Month

--- src/CalendarYear.java
+++ src/CalendarYear.java
@@ -6,6 +6,7 @@ import java.util.GregorianCalendar;
 public class CalendarYear {
     private ArrayList<BitSet> Months;
     private int Year;
+    private ArrayList<Integer> AverageMonths;
 
     public CalendarYear(int Year){
         Months = new ArrayList<>();
@@ -16,11 +17,47 @@ public class CalendarYear {
            BitSet MonthBitSet = new BitSet(days);
            Months.add(MonthBitSet);
         }
+        AverageMonths = new ArrayList<Integer>(31);
+        for(int i=0;i<31;i++){
+            AverageMonths.add(0);
+        }
     }
 
     // Handle Error when Day is out of bounds
-    public CalendarYear flipMonthDay(int Month, int Day){
-        Months.get(Month).flip(Day);
+    public CalendarYear setMonthDay(int Month, int Day){
+        Months.get(Month).set(Day);
+        Integer AvgDay = AverageMonths.get(Day);
+        AvgDay+=1;
+        AverageMonths.set(Day,AvgDay);
+        return this;
+    }
+    public CalendarYear unsetMonthDay(int Month, int Day){
+        Months.get(Month).clear(Day);
+        Integer AvgDay = AverageMonths.get(Day);
+        AvgDay+=1;
+        AverageMonths.set(Day,AvgDay);
+        return this;
+    }
+
+    public ArrayList<Integer> getAverageMonths(){
+        return AverageMonths;
+    }
+
+    public ArrayList<Integer> getMonth(int Month){
+        BitSet bsMonth = Months.get(Month);
+        ArrayList<Integer> intMonth = new ArrayList<>();
+        long compMonth = bsMonth.toLongArray()[0];
+        int isSet = 0;
+        int position = 0;
+        while(compMonth > 0){
+           position +=1;
+           isSet = Math.toIntExact(compMonth % 2);
+           if(isSet != 0){
+              intMonth.add(position - 1);
+           }
+           compMonth/=2;
+        }
+        return intMonth;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
-import java.util.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
        CalendarYear Cy = new CalendarYear(2024);
+       Cy.setMonthDay(2,22).setMonthDay(4,1);
+       Cy.setMonthDay(2, 13);
+       Cy.setMonthDay(2, 1)
+               .setMonthDay(2, 3)
+               .setMonthDay(1, 12);
+
+
+       ArrayList<Integer> intM = Cy.getMonth(2);
+
         System.out.println("");
     }
 }
